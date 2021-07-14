66927af66834cd0c32426510d1d75a7b13b2d3ac
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-07-14 10:00:17 -0500

    Update samu.js

--- samu.js
+++ samu.js
@@ -2963,9 +2963,9 @@ fakee = fs.readFileSync('./src/img.jpg')
 try {
 get_result = await getJson(`https://api.lolhuman.xyz/api/ytplay2?apikey=${api}&query=${query}`)
 get_result = get_result.result
-short = await getJson(`https://tinyurl.com/api-create.php?url=${get_result.video}`)
+short = await getJson(`https://api.lolhuman.xyz/api/shortlink?apikey=${api}&url=${get_result.video}`)
 ini_txt = `🧊Titulo : ${get_result.title}\n\n`
-ini_txt += `_Si el video no llega, puede descargar por aqui_ :\n ${short.body}`
+ini_txt += `_Si el video no llega, puede descargar por aqui_ :\n ${short.result}`
 ini_buffer = await getBuffer(get_result.thumbnail)
 await samu330.sendMessage(from, ini_buffer, image, { quoted: fvid, caption: ini_txt, thumbnail: fakee, contextInfo: {"forwardingScore": 9999, "isForwarded": true} })
 get_video = await getBuffer(get_result.video)
