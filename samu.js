c1d3c31e32ea702af5548200d8a1fb3b407d1efc
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-07-16 16:42:44 -0500

    Update samu.js

--- samu.js
+++ samu.js
@@ -3518,7 +3518,7 @@ if (!isGroup) return reply(mess.only.group)
 if (args.length < 1) return reply('Etiqueta a alguien para utilizar su foto!!!')
 if (sam.message.extendedTextMessage === undefined || sam.message.extendedTextMessage === null) return reply('Etiqueta a alguien')
 mentioned = sam.message.extendedTextMessage.contextInfo.mentionedJid[0]
-id3 = id
+id3 = 'id'
 let {jid, id3, notify } = groupMembers.find(x => x.jid === mentioned)
 try {
 pp = await samu330.getProfilePicture(id3)
