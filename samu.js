e70476e5a47bdefdf57b4ef8f417ef70f845e6ab
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-07-18 12:36:18 -0500

    Update samu.js

--- samu.js
+++ samu.js
@@ -150,7 +150,7 @@ await samu330.blockUser(callerId, "add")
 samu330.on('group-participants-update', async (anu) => {
 if (!welkom.includes(anu.jid)) return
 try {
-const mdata = await samu330.groupMetadata(anu.jid)
+var mdata = await samu330.groupMetadata(anu.jid)
 console.log(anu)
 if (anu.action == 'add') {               
 num = anu.participants[0]
