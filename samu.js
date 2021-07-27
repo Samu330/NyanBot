6095c3ee8779d7f3bd76cb6c99bf788f74a3b3be
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-07-27 12:52:56 -0500

    Update samu.js

--- samu.js
+++ samu.js
@@ -4196,7 +4196,7 @@ break
 case 's2':
 if (((isMedia && !sam.message.videoMessage) || isQuotedImage) && args.length == 0) {
 s2 = isQuotedImage ? JSON.parse(JSON.stringify(sam).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : sam
-samsam = await samu330.downloadAndSaveMediaMessage(cara)
+samsam = await samu330.downloadAndSaveMediaMessage(s2)
 const sticker2 = new WSF.Sticker(samsam, { crop: false, animated: false, pack: '🌐', author: 'Samu330' })
 await sticker2.build()
 const sticBuffer = await sticker2.get()
